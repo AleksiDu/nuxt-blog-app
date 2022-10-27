f49168be0e712e8485818ec2702817de6f8c629d
AleksiDu <aleksi.duluzauri@gmail.com>
2022-10-27 10:35:42 +0400

    start project

---
+++ pages/about/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About me" Page!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam adipisci
+      facere aliquam rerum qui perspiciatis, ut repellat, numquam sed enim
+      consequatur quis officiis quas inventore quaerat voluptatem nesciunt,
+      vitae commodi!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam adipisci
+      facere aliquam rerum qui perspiciatis, ut repellat, numquam sed enim
+      consequatur quis officiis quas inventore quaerat voluptatem nesciunt,
+      vitae commodi!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam adipisci
+      facere aliquam rerum qui perspiciatis, ut repellat, numquam sed enim
+      consequatur quis officiis quas inventore quaerat voluptatem nesciunt,
+      vitae commodi!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
