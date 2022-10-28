0b50c5fafaa50fecde5ff8a8e3759a0f2f5740b0
AleksiDu <aleksi.duluzauri@gmail.com>
2022-10-28 11:28:49 +0400

    add the header component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
+    <NavigationTheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <NavigationTheSidenav
+      :show="displaySidenav"
+      @close="displaySidenav = false"
+    />
     <slot />
   </div>
 </template>
+
+<script setup>
+const displaySidenav = ref(false);
+</script>
 
 <style>
 html {
